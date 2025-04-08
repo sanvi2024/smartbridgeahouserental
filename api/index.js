057caf5a008c1a61026124cd0baae66d7e87bae5
Isha <ishaprajapati112004@gmail.com>
2025-04-09 01:44:52 +0530

    fully-functional-code

---
+++ api/index.js
@@ -0,0 +1,65 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// ✅ Load .env from root directory
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+// console.log('MONGO_URI:', process.env.MONGO_URI);  // Add this
+// console.log('JWT_SECRET:', process.env.JWT_SECRET);  // And this
+
+
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+// const __dirname = path.resolve(); // Removed duplicate declaration
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
